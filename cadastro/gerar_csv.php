667c6f41251c635422f7b0bab5635e184ee7d3c1
Glaison <Glaison26.queiroz@gmail.com>
2025-06-27 11:23:11 -0300

    dev

--- cadastro/gerar_csv.php
+++ cadastro/gerar_csv.php
@@ -2,22 +2,23 @@
 session_start();
 $d_data1=$_SESSION['c_data1'];
 $d_data2=$_SESSION['c_data2'];
-
-//echo "<h1>Gerar Excel - csv</h1>";
 // conexão dom o banco de dados
 include("../conexao.php");
 // Aceitar csv ou texto 
 header('Content-Type: text/csv; charset=utf-8');
-
 header('Content-Disposition: attachment; filename=contatos.csv');
 // Gravar no buffer
-$resultado = fopen("php://output", 'w');
+//$resultado = fopen("php://output", 'w');
+//$caminho_pasta = '/laragon/www/funcionarios/docs/';
+$caminho_pasta = '/docs/envios/';
+$resultado = 'contatos.csv';
+$resultado = $caminho_pasta.$resultado;
+$resultado = fopen($resultado, 'w');
 
 //mb_convert_encoding('Endereço', "ISO-8859-1", "UTF-8")
 
 // Criar o cabeçalho do Excel - Usar a função mb_convert_encoding para converter carateres especiais'
 // faço a Leitura da tabela com sql
-
 $c_sql = "SELECT funcionarios.nome, funcionarios.telefone,  funcionarios.data_nasc, funcionarios.sexo FROM funcionarios
             WHERE (status<>'N') and (MONTH(data_nasc) > MONTH('$d_data1') OR (MONTH(data_nasc) = MONTH('$d_data1') AND DAY(data_nasc) >= DAY('$d_data1')))
             AND (MONTH(data_nasc) < MONTH('$d_data2') OR (MONTH(data_nasc) = MONTH('$d_data2') AND DAY(data_nasc) <= DAY('$d_data2')))
