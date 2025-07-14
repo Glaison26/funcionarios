0a1c70c11ea3895bf1f947aca094a9ad82ca00a6
Glaison <Glaison26.queiroz@gmail.com>
2025-07-14 15:32:58 -0300

    dev

--- cadastro/gerar_csv.php
+++ cadastro/gerar_csv.php
@@ -8,15 +8,10 @@ include("../conexao.php");
 header('Content-Type: text/csv; charset=utf-8');
 header('Content-Disposition: attachment; filename=contatos.csv');
 // Gravar no buffer
-//$resultado = fopen("php://output", 'w');
-//$caminho_pasta = '/laragon/www/funcionarios/docs/';
 $caminho_pasta = '/funcionarios/envio/';
 $resultado = 'contatos.csv';
 $resultado = $caminho_pasta.$resultado;
 $resultado = fopen($resultado, 'w');
-
-//mb_convert_encoding('Endereço', "ISO-8859-1", "UTF-8")
-
 // Criar o cabeçalho do Excel - Usar a função mb_convert_encoding para converter carateres especiais'
 // faço a Leitura da tabela com sql
 $c_sql = "SELECT funcionarios.nome, funcionarios.telefone,  funcionarios.data_nasc, funcionarios.sexo FROM funcionarios
@@ -31,34 +26,23 @@ $cabecalho = [
     'Data Nascimento',
     'Sexo'
 ];
-
-
 // verifico se a query foi correto
 if (!$result) {
     die("Erro ao Executar Sql!!" . $conection->connect_error);
 }
-
-// Array de dados
-// insiro os registro do banco de dados na tabela 
-while ($c_linha2 = $result->fetch_assoc()) {
-    $c_linha2 = str_replace('(31)','',$c_linha2);
-    fputcsv($resultado, $c_linha2, ';');
-}
-// Fechar arquivo
-fclose($resultado);
-$c_sql = "SELECT funcionarios.nome, funcionarios.telefone,  funcionarios.data_nasc, funcionarios.sexo FROM funcionarios
-            WHERE (status<>'N') and (MONTH(data_nasc) > MONTH('$d_data1') OR (MONTH(data_nasc) = MONTH('$d_data1') AND DAY(data_nasc) >= DAY('$d_data1')))
-            AND (MONTH(data_nasc) < MONTH('$d_data2') OR (MONTH(data_nasc) = MONTH('$d_data2') AND DAY(data_nasc) <= DAY('$d_data2')))
-            ORDER BY MONTH(data_nasc), DAY(data_nasc)";
-            //echo $c_sql;
-$result2 = $conection->query($c_sql);
 // limpo a tabela de envio para nova gravação
 $c_sql_del = "delete from envio";
 $result_del = $conection->query($c_sql_del);
-// gero resultado na tabela de envio do bancos de dados
-while ($c_linha2 = $result2->fetch_assoc()) {
-    // insiro registro via sql
+// Array de dados
+// insiro os registro do banco de dados na tabela e gerro registros no arquivo csv
+while ($c_linha2 = $result->fetch_assoc()) {
+    $c_linha2 = str_replace('(31)','',$c_linha2);
+    // arquivo csv
+    fputcsv($resultado, $c_linha2, ';');
+     // insiro registro via sql
     $c_sql2 = "Insert into envio (nome,telefone,data,sexo) value ('$c_linha2[nome]', '$c_linha2[telefone]', '$c_linha2[data_nasc]', '$c_linha2[sexo]')";
     $result3 = $conection->query($c_sql2);
 }
+// Fechar arquivo
+fclose($resultado);
 ?>
